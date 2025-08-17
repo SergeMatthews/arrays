3da4f06bc2a3166f2be2532389539de94f55d431
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-17 13:45:51 +0600

    Add files via upload

---
+++ arrays1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int arr[] {12, 32, 36, 21, 75, 45, 1, 0, 25, 31};
+    for (int i = 0; i < sizeof(arr)/sizeof(*arr); ++i)
+    {
+        cout << arr[i];
+        if (i == (sizeof(arr) / sizeof(*arr) - 1))
+            cout << endl;
+        else
+            cout << ", ";
+    }
+
+    return 0;
+}
+
+
