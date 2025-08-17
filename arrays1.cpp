94da887b5327cf2513291cc7fffc714c8be22445
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-17 13:47:19 +0600

    Update arrays1.cpp

--- arrays1.cpp
+++ arrays1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -6,10 +6,11 @@ int main()
     setlocale(LC_ALL, "RU");
 
     int arr[] {12, 32, 36, 21, 75, 45, 1, 0, 25, 31};
-    for (int i = 0; i < sizeof(arr)/sizeof(*arr); ++i)
+    int arr_size = sizeof(arr) / sizeof(*arr);
+    for (int i = 0; i < arr_size; ++i)
     {
         cout << arr[i];
-        if (i == (sizeof(arr) / sizeof(*arr) - 1))
+        if (i == arr_size - 1)
             cout << endl;
         else
             cout << ", ";
@@ -17,5 +18,3 @@ int main()
 
     return 0;
 }
-
-
